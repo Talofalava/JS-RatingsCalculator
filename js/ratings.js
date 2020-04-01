85589fccd77386fb0cf0c552717e3c4998a733e7
Talofalava <62039289+Talofalava@users.noreply.github.com>
2020-04-01 08:13:35 -0600

    Completed module

---
+++ js/ratings.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+function collect_ratings (){
+  let ratings = {
+    count: 0,
+    sum: 0,
+    average: 0,
+  }
+
+  let rating = 0;
+}
+
+const elements = document.querySelectorAll(".rating");
+
+
+elements.forEach(function(rating) {
+  console.log(rating);
+})
