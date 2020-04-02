902ae78aab344374728a9827cfcba97f7f8e3414
Talofalava <62039289+Talofalava@users.noreply.github.com>
2020-04-02 10:00:15 -0600

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -25,5 +25,6 @@ const collect_ratings = () => {
 document.addEventListener('change', () => {
     const ratings = collect_ratings();
 
+    document.querySelector('#average').value = ratings.average.toFixed(2);
 
 })
